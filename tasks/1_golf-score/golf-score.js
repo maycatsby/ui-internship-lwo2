29ef485126a4f7ff87bd7b2d43198696ac7612bd
SofiMat <sofiyamatviishyn@gmail.com>
2018-10-17 13:16:04 +0300

    Eslint comment disabled

--- tasks/1_golf-score/golf-score.js
+++ tasks/1_golf-score/golf-score.js
@@ -1,3 +1,5 @@
+/* eslint "require-jsdoc": 0 */
+
 export const GOLF_SCORES = {
   HOLE_IN_ONE: 'Hole-in-one!',
   EAGLE: 'Eagle',
@@ -10,29 +12,21 @@ export const GOLF_SCORES = {
 
 
 export function golfScore(par, stroke) {
-
-    if (stroke === par-1){
-        return 'Birdie';
-    }
-    else if (stroke === par+1){
-        return 'Bogey';
-    }
-    else if(stroke <= par - 2 && stroke !== 1){
-        return 'Eagle';
-    }
-    else if(stroke === par+2){
-        return 'Double Bogey';
-    }
-    else if (stroke >= par+3){
-            return 'Go Home!';
-          }
-    else if (stroke === par && stroke !== 1){
-        return 'Par';
-    }      
-    else if (stroke === 1){
-        return 'Hole-in-one!';
-    }
-    else{
-        return 'Go Home!';
-    }
+  if (stroke === par - 1) {
+    return 'Birdie';
+  } else if (stroke === par + 1) {
+    return 'Bogey';
+  } else if (stroke <= par - 2 && stroke !== 1) {
+    return 'Eagle';
+  } else if (stroke === par + 2) {
+    return 'Double Bogey';
+  } else if (stroke >= par + 3) {
+    return 'Go Home!';
+  } else if (stroke === par && stroke !== 1) {
+    return 'Par';
+  } else if (stroke === 1) {
+    return 'Hole-in-one!';
+  } else {
+    return 'Go Home!';
+  }
 }
