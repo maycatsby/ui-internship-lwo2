ab4914e303431844639cd29f3eca57c73e852c30
YarynaD <ydemkiv@griddynamics.com>
2018-10-17 17:55:03 +0300

    task2-3

---
+++ tasks/2_convert-to-f/convert-to-f.js
@@ -0,0 +1,4 @@
+export function convertToF(C) {
+  let F = C*9/5 + 32;
+  return F;
+}
---
+++ tasks/3_is-boolean/is-boolean.js
@@ -0,0 +1,9 @@
+/* eslint "require-jsdoc": 0 */
+
+export function isBoolean(variable) {
+  if (typeof(variable)=== 'boolean') {
+    return true;
+  } else {
+    return false;
+  }
+}
