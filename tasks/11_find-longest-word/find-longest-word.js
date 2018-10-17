21a6a0b02907e8e29b92db9670cd6a50037199cd
EdGrLab <grabchak1999@gmail.com>
2018-10-17 18:00:56 +0300

    smth

--- tasks/11_find-longest-word/find-longest-word.js
+++ tasks/11_find-longest-word/find-longest-word.js
@@ -1,10 +1,10 @@
-export function findLongestWord(str){
-    let strCut = str.split(" ");
-    let longestStr = 0;
-    for(let i = 0; i < strCut.length; i++){
-        if(strCut[i].length > longestStr){
-            longestStr = strCut[i].length;
-        }
+export function findLongestWord(str) {
+  let strCut = str.split(' ');
+  let longestStr = 0;
+  for (let i = 0; i < strCut.length; i++) {
+    if (strCut[i].length > longestStr) {
+      longestStr = strCut[i].length;
     }
-    return longestStr;
+  }
+  return longestStr;
 }
