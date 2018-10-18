3ca0531d9cec4b2986d01266cda381d8c31454fa
Andrew Zhukevych <34597767+Hollister009@users.noreply.github.com>
2018-10-18 10:22:35 +0300

    find-number.js changed let to const

--- tasks/9_find-numbers/find-numbers.js
+++ tasks/9_find-numbers/find-numbers.js
@@ -1,7 +1,7 @@
 export const getNumbers = (str) => {
   const reg = /\d+/g;
-  let strArr = str.match(reg);
-  let numArr = strArr.map((el) => {
+  const strArr = str.match(reg);
+  const numArr = strArr.map((el) => {
     return Number(el);
   });
   return numArr;
