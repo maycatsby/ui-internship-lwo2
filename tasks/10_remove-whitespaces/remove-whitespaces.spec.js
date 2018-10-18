357842550a2d0cfa7c3376b00d914aaf4005fb6e
Andrew Zhukevych <34597767+Hollister009@users.noreply.github.com>
2018-10-18 10:20:57 +0300

    Update remove-whitespaces.spec.js

--- tasks/10_remove-whitespaces/remove-whitespaces.spec.js
+++ tasks/10_remove-whitespaces/remove-whitespaces.spec.js
@@ -2,14 +2,10 @@ import {trimWhiteSpaces} from './remove-whitespaces';
 
 describe('remove-whitespaces', () => {
   test('trimWhiteSpaces(‘I am travelling down the river’) should be equal to “Iamtravellingdowntheriver”', () => {
-    expect(trimWhiteSpaces('I am travelling down the river')).toBe(
-        'Iamtravellingdowntheriver'
-    );
+    expect(trimWhiteSpaces('I am travelling down the river')).toBe('Iamtravellingdowntheriver');
   });
 
   test('trimWhiteSpaces(‘  I  am  travelling  down  the  river  ’) should be equal to "Iamtravellingdowntheriver"', () => {
-    expect(trimWhiteSpaces('  I  am  travelling  down  the  river  ')).toBe(
-        'Iamtravellingdowntheriver'
-    );
+    expect(trimWhiteSpaces('  I  am  travelling  down  the  river  ')).toBe('Iamtravellingdowntheriver');
   });
 });
