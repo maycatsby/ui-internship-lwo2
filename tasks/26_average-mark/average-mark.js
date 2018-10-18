00c6376a2830162b7512fde5c29aff86a487d249
SofiMat <sofiyamatviishyn@gmail.com>
2018-10-18 18:59:38 +0300

    task 26

---
+++ tasks/26_average-mark/average-mark.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+
+export function getAverage(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    let avg = sum / arr.length;
+    return Math.floor(avg);
+}
