4439adbec2f3ba4c38faaed15f518118c759ec37
FabianMoretti <dp120793tpi@gmail.com>
2018-10-18 13:39:49 +0300

    dfdf

---
+++ tasks/11_find-longest-word/find-longest-word.js
@@ -0,0 +1,12 @@
+/* eslint "require-jsdoc": 0 */
+
+export const findLongestWord = (str) => {
+  var strSplit = str.split(' ');
+  var longestWord = 0;
+  for(var i = 0; i < strSplit.length; i++){
+    if(strSplit[i].length > longestWord){
+	longestWord = strSplit[i].length;
+     }
+  }
+  return longestWord;
+}
