8b43426344d05ec4c1a49c69e3ae70f75ed9467c
YarynaD <ydemkiv@griddynamics.com>
2018-10-19 17:29:50 +0300

    Tasks_21_30

---
+++ tasks/21_check-letters/check-letters.js
@@ -0,0 +1,10 @@
+export function checkLetters(arr) {
+  let test = arr[1].toLowerCase();
+  let target = arr[0].toLowerCase();
+  for (let i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) === -1) {
+      return false;
+    }
+  }
+  return true;
+}
