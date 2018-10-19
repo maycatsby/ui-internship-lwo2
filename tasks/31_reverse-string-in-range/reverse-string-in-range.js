49dbaffbf99cd32ef8d6e3baae3ad94e4c2955de
SofiMat <sofiyamatviishyn@gmail.com>
2018-10-19 13:25:06 +0300

    task 31

---
+++ tasks/31_reverse-string-in-range/reverse-string-in-range.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+   export function reverseStringInRange (str, arr) {
+        let copyStr = str.split('');
+        let str_len = str.length;
+        let chunk = copyStr.slice(arr[0],arr[1]+1).reverse().join('');
+        copyStr.splice(arr[0],chunk.length,chunk);
+        let final = copyStr.join('');
+        return final;
+    }
+
