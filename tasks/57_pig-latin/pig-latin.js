e1c429b2051abb7b3fb0850964dab3c04e72209a
Andriy Zhukevych <reactive.adnrew@yahoo.com.ua>
2018-10-22 19:07:13 +0300

    unimportant change

--- tasks/57_pig-latin/pig-latin.js
+++ tasks/57_pig-latin/pig-latin.js
@@ -2,4 +2,5 @@ export const translatePigLatin = (string) => {
   const vowels = ['a', 'e', 'i', 'o', 'u'];
   if (vowels.includes(string[0])) return string + 'way';
   let newStr = string.split('');
+  return newStr;
 };
