db59d527fcf6c90a2fdd55f1bd4fe637395bdc62
Andriy Zhukevych <reactive.adnrew@yahoo.com.ua>
2018-10-22 11:20:20 +0300

    Completed sort-by-bits

---
+++ tasks/41_sort-by-bits/sort-by-bits.js
@@ -0,0 +1,8 @@
+export const sortByBits = (array) => {
+  array.sort((a, b) => {
+    const order = (a.toString(2).match(/1/g) || '').length -
+                  (b.toString(2).match(/1/g) || '').length;
+    return order !== 0 ? order : a - b;
+  });
+  return array;
+};
