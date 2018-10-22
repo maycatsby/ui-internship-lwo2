8464cdc502b79651157f3d3701cbb37131075cb5
SofiMat <sofiyamatviishyn@gmail.com>
2018-10-22 23:12:28 +0300

    upd

--- tasks/45_equal-sides/equal-sides.js
+++ tasks/45_equal-sides/equal-sides.js
@@ -1,17 +1,15 @@
 export function findEqualIndex(arr) {
   const arrLeng = arr.length;
-  let leftsum = 0;
-  let rightsum = 0;
+  let leftsum;
+  let rightsum;
   for (let i = 0; i < arrLeng; ++i) {
+    leftsum = 0;
+    rightsum = 0;
     for (let j = 0; j < i; j++) {
-      leftsum = arr.reduce(function(leftsum, arr) {
-        return leftsum + arr[j];
-      }, j);
+      leftsum += arr[j];
     }
     for (let j = i + 1; j < arrLeng; j++) {
-      rightsum = arr.reduce(function(rightsum, arr) {
-        return leftsum + arr[j];
-      }, j);
+      rightsum += arr[j];
     }
     if (leftsum == rightsum) {
       return i;
