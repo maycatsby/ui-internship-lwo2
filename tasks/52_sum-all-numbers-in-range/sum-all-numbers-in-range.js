96faa9762986edd7b0d75e563d0f5be41e5ca2ba
Andrew Zhukevych <hipnos.az@gmail.com>
2018-10-23 00:02:02 +0300

    solved: 52 & 54

--- tasks/52_sum-all-numbers-in-range/sum-all-numbers-in-range.js
+++ tasks/52_sum-all-numbers-in-range/sum-all-numbers-in-range.js
@@ -1,3 +1,11 @@
-export const name = (arg) => {
-  return arg;
+export const sumAll = (arg) => {
+  const firstNum = Math.min(...arg);
+  const lastNum = Math.max(...arg);
+  let newArr = [firstNum];
+  let numI = firstNum + 1;
+  while (numI <= lastNum) {
+    newArr.push(numI);
+    numI++;
+  }
+  return newArr.reduce((sum, cur) => sum + cur);
 };
--- tasks/54_roman-numbers-convert/roman-numbers-convert.js
+++ tasks/54_roman-numbers-convert/roman-numbers-convert.js
@@ -4,9 +4,18 @@ export const convertToRoman = (arg) => {
     500: 'D', 400: 'CD',
     100: 'C', 90: 'XC',
     50: 'L', 40: 'XL',
-    10: 'X', 5: 'V',
-    4: 'IV', 1: 'I',
+    10: 'X', 9: 'IX',
+    5: 'V', 4: 'IV', 1: 'I',
   };
+  const romaKeys = Object.keys(romansNum).reverse();
+  let romanaized = '';
+  let numCp = arg;
 
-  return romansNum;
+  for (let value of romaKeys) {
+    while (+value <= numCp) {
+      romanaized += romansNum[value];
+      numCp -= +value;
+    }
+  }
+  return romanaized;
 };
