a55da52d907e2c82eb9dc65b3b526b2d22da74e3
EdGrLab <grabchak1999@gmail.com>
2018-10-23 14:24:57 +0300

    fixed task 37

--- tasks/37_min-number-to-prime/min-number-to-prime.js
+++ tasks/37_min-number-to-prime/min-number-to-prime.js
@@ -1,16 +1,17 @@
 export function minNumberToPrime(arr) {
   let sumOfAll = arr.reduce((sum, curr) => sum + curr);
-  let result = 0;
-  let check = true;
-  while (check) {
-    for (let i = sumOfAll - 1; i > 2; i--) {
-      if (sumOfAll % i == 0) check = false;
+  function isPrime(value) {
+    for (let x = 2; x < value; x++) {
+      if (value % x === 0) {
+        return false;
+      }
     }
-    if (check != true) {
-      sumOfAll++;
-      result ++;
-      check = true;
-    } else break;
+    return true;
   }
-  return result;
+  let count = 0;
+  if (isPrime(sumOfAll)) return 0;
+  while (!isPrime(sumOfAll)) {
+    count++; sumOfAll++;
+  }
+  return count;
 }
