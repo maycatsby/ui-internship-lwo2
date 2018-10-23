000458817f234a3f9c6d329b59e3ea70d1fb94f2
Sophia Matviishyn <sofiyamatviishyn@gmail.com>
2018-10-23 16:30:02 +0300

    add space

--- tasks/52_sum-all-numbers-in-range/sum-all-numbers-in-range.js
+++ tasks/52_sum-all-numbers-in-range/sum-all-numbers-in-range.js
@@ -2,7 +2,7 @@ export function sumAll(arr) {
   const max = Math.max(arr[0], arr[1]);
   const min = Math.min(arr[0], arr[1]);
   let final = 0;
-  for (let i=min; i <= max; i++) {
+  for (let i = min; i <= max; i++) {
     final += i;
   }
   return final;
