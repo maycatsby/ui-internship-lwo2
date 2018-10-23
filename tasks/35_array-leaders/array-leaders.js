6efa4f0f895b3fc72c1063e6ed8192914e8bd832
NFalinska <nataliia.falinska@gmail.com>
2018-10-23 19:28:51 +0300

    fixes

--- tasks/35_array-leaders/array-leaders.js
+++ tasks/35_array-leaders/array-leaders.js
@@ -2,7 +2,7 @@ export function arrayLeaders(arr) {
   arr.push(0, 0);
   const leaders = [];
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > arr[i + 1] + arr[i + 2]) {
+    if (arr[i] > arr.slice(i + 1).reduce((a, b) => a + b, 0)) {
       leaders.push(arr[i]);
     }
   }
