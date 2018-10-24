9f107384be3f2bea9773802c21b509556e97ae5b
Andriy Zhukevych <reactive.adnrew@yahoo.com.ua>
2018-10-24 11:31:23 +0300

    finished all tasks in branch

--- tasks/62_swap-cases/swap-cases.js
+++ tasks/62_swap-cases/swap-cases.js
@@ -1,7 +1,9 @@
 export const swapCases = (swap) => {
   const lettersArr = swap.split('');
   return lettersArr.map((el) => {
-    if (/[A-Z]/.test(el)) return el.toLowerCase();
+    if (/[A-Z]/.test(el)) {
+      return el.toLowerCase();
+    }
     return el.toUpperCase();
   }).join('');
 };
--- tasks/65_smaller-common-multiple/smaller-common-multiple.js
+++ tasks/65_smaller-common-multiple/smaller-common-multiple.js
@@ -1,3 +1,15 @@
-export const smallestCommons = (range) => {
-  return range;
+export const smallestCommons = (arr) => {
+  const rangeArr = [Math.min(...arr)];
+  let num = rangeArr[0] + 1;
+  while (num <= Math.max(...arr)) {
+    rangeArr.push(num);
+    num++;
+  }
+  let curN = Math.max(...rangeArr) * 2;
+  let condition = rangeArr.every((el) => el % curN === 0);
+  while (!condition) {
+    curN += rangeArr[rangeArr.length - 1];
+    condition = rangeArr.every((el) => curN % el === 0);
+  }
+  return curN;
 };
