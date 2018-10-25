3ecac2050b53f3cad0f80e0869bdd20ba6f5a315
Nataliya Mykytyuk <41234940+dnistrova@users.noreply.github.com>
2018-10-25 15:10:57 +0300

    Update abbreviated-name.js

--- tasks/51_abbreviated-name/abbreviated-name.js
+++ tasks/51_abbreviated-name/abbreviated-name.js
@@ -2,7 +2,7 @@ export function abbreviated(str) {
   const splitName = str.split(' ');
   if (splitName.length >= 2) {
     for (let i = 1; i < splitName.length; i++) {
-      splitName[i]=splitName[i].charAt(0)+'.';
+      splitName[i] = splitName[i].charAt(0) + '.';
     }
   }
   return splitName.join(' ');
