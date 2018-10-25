f671d0c9d755af757741b65867feac318e2baf8b
Andrew Zhukevych <34597767+Hollister009@users.noreply.github.com>
2018-10-25 15:42:03 +0300

    Update smaller-common-multiple.js

--- tasks/65_smaller-common-multiple/smaller-common-multiple.js
+++ tasks/65_smaller-common-multiple/smaller-common-multiple.js
@@ -1,14 +1,15 @@
 export const smallestCommons = (arr) => {
   const rangeArr = [Math.min(...arr)];
+  const maxNum = Math.max(...arr);
   let num = rangeArr[0] + 1;
-  while (num <= Math.max(...arr)) {
+  while (num <= maxNum) {
     rangeArr.push(num);
     num++;
   }
-  let curN = Math.max(...rangeArr) * 2;
+  let curN = maxNum * 2;
   let condition = rangeArr.every((el) => el % curN === 0);
   while (!condition) {
-    curN += rangeArr[rangeArr.length - 1];
+    curN += maxNum;
     condition = rangeArr.every((el) => curN % el === 0);
   }
   return curN;
