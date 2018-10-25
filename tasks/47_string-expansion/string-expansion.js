c8acf847701677ad3d49e929315fb4beafec485a
NFalinska <nataliia.falinska@gmail.com>
2018-10-25 14:41:30 +0300

    tasks_41_50

--- tasks/47_string-expansion/string-expansion.js
+++ tasks/47_string-expansion/string-expansion.js
@@ -1,28 +1,16 @@
 export function stringExpansion(str) {
+  let res = '';
+  let number = 1;
   let strArr = str.split('').map((el) => {
     let num = Number(el);
     return num === 0 ? num : num || el;
   });
-  const newArr =[];
-  if (typeof strArr[0] === 'string') {
-    return str;
-  }
   for (let i = 0; i < strArr.length; i++) {
-    if (typeof strArr[i-1] ==='number') {
-      newArr.push(strArr[i].repeat(strArr[i-1]));
-    } else if (typeof strArr[i-1]!== 'number') {
-      strArr.shift(strArr[i-1]);
+    if (typeof strArr[i] === 'number') {
+      number = strArr[i];
+    } else {
+      res += strArr[i].repeat(number);
     }
-    //   let del= strArr.shift(strArr[i+1]);
-    //   console.log(strArr);
-    //   console.log(del);
-    //   console.log(strArr.length);
-    // } else if (typeof strArr[i-1] === 'number') {
-    //   newArr.push(strArr[i].repeat(strArr[0]));
-    // }
-  } 
-  while(typeof strArr[i-1]!=== 'number') {
-
   }
-  return newArr.join('');
+  return res;
 }
