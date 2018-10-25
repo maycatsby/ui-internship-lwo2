22a9f10fc6ff058591ff9ffe86937425af62855f
Nataliya Mykytyuk <41234940+dnistrova@users.noreply.github.com>
2018-10-25 15:18:37 +0300

    Update search-and-replace.js

--- tasks/56_search-and-replace/search-and-replace.js
+++ tasks/56_search-and-replace/search-and-replace.js
@@ -3,7 +3,5 @@ export function myReplace(str, before, after) {
   if (str[index] === str[index].toUpperCase()) {
     after = after.charAt(0).toUpperCase() + after.slice(1);
   }
-  str = str.replace(before, after);
-
-  return str;
+  return str.replace(before, after);
 }
