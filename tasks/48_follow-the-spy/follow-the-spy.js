5111bffbe851a9fb981cf72cfda63281bcec40b1
NFalinska <nataliia.falinska@gmail.com>
2018-10-25 14:48:43 +0300

    some fix

--- tasks/48_follow-the-spy/follow-the-spy.js
+++ tasks/48_follow-the-spy/follow-the-spy.js
@@ -1,7 +1,7 @@
 export function followTheSpy(arr) {
-  let stop = [arr[0][0], arr[0][1]];
+  const stop = [arr[0][0], arr[0][1]];
   for (let i = 1; i < arr.length; i++) {
-    let nextStop = arr.filter((el) => el[0] === stop[i]);
+    const nextStop = arr.filter((el) => el[0] === stop[i]);
     stop.push(nextStop[0][1]);
   }
   return stop.join(', ');
