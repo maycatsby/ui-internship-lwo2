8bfd0f13d866a25b8c887bdfd2fa9c8dabf8ed1d
YarynaD <ydemkiv@griddynamics.com>
2018-10-26 10:01:18 +0300

    Task 51 corrected

--- tasks/51_abbreviated-name/abbreviated-name.js
+++ tasks/51_abbreviated-name/abbreviated-name.js
@@ -1,12 +1,8 @@
 export function abbreviated(str) {
   const arr = str.split(' ');
-  let arrNew = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (i === 0) {
-      arrNew.push(arr[0]);
-    } else {
-      arrNew.push(arr[i][0] + '.');
-    }
+  let arrNew = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    arrNew.push(arr[i][0] + '.');
   }
   return arrNew.join(' ');
 }
