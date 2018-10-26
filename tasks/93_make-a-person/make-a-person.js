ea1bc7749725c1848c618a95e6fbe69499657ad4
SofiMat <sofiyamatviishyn@gmail.com>
2018-10-26 16:22:41 +0300

    93 upd

--- tasks/93_make-a-person/make-a-person.js
+++ tasks/93_make-a-person/make-a-person.js
@@ -19,19 +19,13 @@ export class Person {
   }
   setFirstName(first) {
     this.firstName = first;
-
-    return this.firstName;
   }
   setLastName(last) {
     this.lastName = last;
-
-    return this.lastName;
   }
   setFullName(firstAndLast) {
     this.splitted = firstAndLast.split(' ');
     this.firstName = this.splitted[0];
     this.lastName = this.splitted[1];
-
-    return this.firstName + ' ' + this.lastName;
   }
 }
