9446ea0737fd1d568cb40eba845d3aeb62583097
NFalinska <nataliia.falinska@gmail.com>
2018-10-30 00:35:34 +0200

    some fix

--- tasks/75_complementary-dna/complementary-dna.js
+++ tasks/75_complementary-dna/complementary-dna.js
@@ -1,25 +1,24 @@
 export function DNAStrand(str) {
-    let paired = [];
-    let search = function(char) {
-      switch (char) {
-        case 'A':
-          paired.push(['T']);
-          break;
-        case 'T':
-          paired.push(['A']);
-          break;
-        case 'C':
-          paired.push(['G']);
-          break;
-        case 'G':
-          paired.push(['C']);
-          break;
-      }
-    };
-    for (let i = 0; i < str.length; i++) {
-      search(str[i]);
+  const paired = [];
+  let search = function(char) {
+    switch (char) {
+      case 'A':
+        paired.push(['T']);
+        break;
+      case 'T':
+        paired.push(['A']);
+        break;
+      case 'C':
+        paired.push(['G']);
+        break;
+      case 'G':
+        paired.push(['C']);
+        break;
     }
-  
-    return paired.join('');
+  };
+  for (let i = 0; i < str.length; i++) {
+    search(str[i]);
   }
-  
+
+  return paired.join('');
+}
