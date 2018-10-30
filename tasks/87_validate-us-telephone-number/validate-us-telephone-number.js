7a7812faf66ff4e2d3bc3ba19eb883609d5d644f
NFalinska <nataliia.falinska@gmail.com>
2018-10-30 21:53:32 +0200

    some fix

--- tasks/87_validate-us-telephone-number/validate-us-telephone-number.js
+++ tasks/87_validate-us-telephone-number/validate-us-telephone-number.js
@@ -1,6 +1,6 @@
 /* eslint 'require-jsdoc': 0 */
 
 export function telephoneCheck(str) {
-  let regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+  let regex = /^\s*1?\s*((\(\d{3}\))|(\d{3}))\s*-?\s*(\d{3})\s*-?\s*(\d{4})\s*$/;
   return regex.test(str);
 }
