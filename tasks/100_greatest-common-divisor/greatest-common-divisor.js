0159c7203754367114c55d7c75f3aef955e29a47
YarynaD <ydemkiv@griddynamics.com>
2018-10-30 17:11:38 +0200

    Tasks 93-104

---
+++ tasks/100_greatest-common-divisor/greatest-common-divisor.js
@@ -0,0 +1,13 @@
+export function greatestCommonDivisor(a, b) {
+  if (a === 0) {
+    return b;
+  }
+  while (b!=0) {
+    if (a > b) {
+      a -= b;
+    } else {
+      b -= a;
+    }
+  }
+  return a;
+}
