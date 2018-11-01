5373d57da7222f976ab21f1b854cd9f36f2947ad
NFalinska <nataliia.falinska@gmail.com>
2018-11-01 12:48:58 +0200

    Added 84 and 85

--- tasks/47_string-expansion/string-expansion.spec.js
+++ tasks/47_string-expansion/string-expansion.spec.js
@@ -2,24 +2,24 @@ import { stringExpansion } from './string-expansion';
 
 describe('String expansion', () => {
 
-    // test("stringExpansion('3D2a5d2f') === 'DDDaadddddff'", () => {
-    //     expect(stringExpansion('3D2a5d2f')).toBe("DDDaadddddff");
-    // });
+    test("stringExpansion('3D2a5d2f') === 'DDDaadddddff'", () => {
+        expect(stringExpansion('3D2a5d2f')).toBe("DDDaadddddff");
+    });
 
     test("stringExpansion('3abc') === 'aaabbbccc'", () => {
         expect(stringExpansion('3abc')).toBe("aaabbbccc");
     });
 
-    // test("stringExpansion('3d332f2a') === 'dddffaa'", () => {
-    //     expect(stringExpansion('3d332f2a')).toBe("dddffaa");
-    // });
+    test("stringExpansion('3d332f2a') === 'dddffaa'", () => {
+        expect(stringExpansion('3d332f2a')).toBe("dddffaa");
+    });
 
-    // test("stringExpansion('abcde') === 'abcde'", () => {
-    //     expect(stringExpansion('abcde')).toBe("abcde");
-    // });
+    test("stringExpansion('abcde') === 'abcde'", () => {
+        expect(stringExpansion('abcde')).toBe("abcde");
+    });
 
-    // test("stringExpansion('') === ''", () => {
-    //     expect(stringExpansion('')).toBe("");
-    // });
+    test("stringExpansion('') === ''", () => {
+        expect(stringExpansion('')).toBe("");
+    });
 
 });
