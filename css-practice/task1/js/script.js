d3e5c3b2b62c5daf3bf354b9df6483a7db8e576a
moskdev <molchanovsk@gmail.com>
2018-11-07 14:27:19 +0200

    fixed images names, css properties order, minor compatibility issues

--- css-practice/task1/js/script.js
+++ css-practice/task1/js/script.js
@@ -5,10 +5,10 @@ function dropDownMenu() {
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
-    let dropdowns = document.getElementsByClassName('dropdown-content');
-    let i;
+    var dropdowns = document.getElementsByClassName('dropdown-content');
+    var i;
     for (i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
+      var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
