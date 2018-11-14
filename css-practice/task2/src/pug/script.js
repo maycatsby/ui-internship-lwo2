427012e4a8f35ee9f9343fb76b0116543434d2a5
Andrew Zhukevych <hipnos.az@gmail.com>
2018-11-14 22:36:23 +0200

    latest updates

--- css-practice/task2/src/pug/script.js
+++ css-practice/task2/src/pug/script.js
@@ -9,13 +9,11 @@
 
   menuBtn.addEventListener('click', () => {
     if (!menuShow) {
-      menuBtn.classList.add('close');
       navMenu.classList.add('show');
     } else {
-      menuBtn.classList.remove('close');
       navMenu.classList.remove('show');
     }
     // change navMenu state
     menuShow = !menuShow;
-  })
+  });
 })(document);
