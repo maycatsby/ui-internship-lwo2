a51861c26dc5ea0379854733805a2c189cd88aa5
EdGrLab <grabchak1999@gmail.com>
2018-11-26 15:32:34 +0200

    fixed issues

--- validation/index.js
+++ validation/index.js
@@ -1,43 +1,52 @@
 /* eslint-disable */
-const menu = document.querySelector(".menu-btn");
-const hiddenMenu = document.querySelector(".burger-menu");
+const menu = document.querySelector('.menu-btn');
+const hiddenMenu = document.querySelector('.burger-menu');
 menu.onclick = () => {
-  hiddenMenu.classList.contains("d-none")
-    ? hiddenMenu.classList.remove("d-none")
-    : hiddenMenu.classList.add("d-none");
+  hiddenMenu.classList.contains('d-none')
+    ? hiddenMenu.classList.remove('d-none')
+    : hiddenMenu.classList.add('d-none');
 };
 window.onclick = e => {
   if (e.target !== menu) {
-    hiddenMenu.classList.add("d-none");
+    hiddenMenu.classList.add('d-none');
   }
 };
 
 function validate(id, pattern) {
   const node = document.getElementById(id);
+  let tip = document.getElementById('tip');
 
   if (pattern.test(node.value)) {
-    node.classList.add("success");
-    document.getElementById("tip").style.display = "none";
+    node.classList.add('success');
+    document.getElementById('tip').style.display = 'none';
     return;
   }
-  node.classList.remove("success");
-  node.classList.add("error");
-  document.getElementById("tip").style.display = "block";
+  else if (!node.value) {
+      tip.innerHTML = 'Fill in ' + id + ' field!';
+    node.classList.add('error');
+    tip.style.display = 'block';
+  } else {
+  node.classList.remove('success');
+  node.classList.add('error');
+  tip.innerHTML = 'Fill in ' + id + ' correctly!';
+  tip.style.display = 'block';
+  }
+
 }
 
 function switchTab(e, tab) {
   let tabcontent;
   let tablinks;
-  tabcontent = document.getElementsByClassName("info-text-items");
+  tabcontent = document.getElementsByClassName('info-text-items');
   for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+    tabcontent[i].style.display = 'none';
   }
-  tablinks = document.getElementsByClassName("btn-info");
+  tablinks = document.getElementsByClassName('btn-info');
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace("current", "");
+    tablinks[i].className = tablinks[i].className.replace('current', '');
   }
-  document.getElementById(tab).style.display = "block";
-  e.currentTarget.className += " current";
+  document.getElementById(tab).style.display = 'block';
+  e.currentTarget.className += ' current';
 }
 
-document.getElementById("defaultTab").click();
+document.getElementById('defaultTab').click();
