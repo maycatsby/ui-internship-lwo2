e5a99d6e39960b1f01440055915a31e660c81925
YarynaD <ydemkiv@griddynamics.com>
2018-11-27 16:56:57 +0200

    changes request

--- css-practice/task_2_dynamic/forms.js
+++ css-practice/task_2_dynamic/forms.js
@@ -4,47 +4,19 @@ const mailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-
 const passReg = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/;
 const subjReg = /.{1,30}/;
 const textAreaReg = /.{1,250}/;
-const userName = document.forms['headerForm']['user_name'];
-const userNameFooter = document.forms['footerForm']['user_name'];
-const userEmail = document.forms['headerForm']['user_mail'];
-const userEmailFooter = document.forms['footerForm']['user_mail'];
-const userPassword = document.forms['headerForm']['user_password'];
-const messageSubject = document.forms['footerForm']['subject'];
-const theMessage = document.forms['footerForm']['message'];
 
-myColorFunc = function(testRule, objectToTest) {
-  if (testRule.test(objectToTest.value)) {
-    objectToTest.style.outline = '2px solid green';
+const test_rules = {'user_name':nameReg, 'user_mail':mailReg, 'user_password':passReg, 'subject':subjReg, 'message':textAreaReg};
+
+const myForm = document.querySelectorAll('form,input,textarea');
+
+newColorFunc = function() {
+  if (test_rules[this.name].test(this.value)) {
+    this.style.outline = '2px solid green';
   } else {
-    objectToTest.style.outline = '2px solid red';
-  }  
+    this.style.outline = '2px solid red';
+  }
 };
 
-userName.onblur = function() {
-  myColorFunc(nameReg, userName);
-};
-
-userNameFooter.onblur = function() {
-  myColorFunc(nameReg, userNameFooter);
-};
-
-userEmail.onblur = function() {
-  myColorFunc(mailReg, userEmail);
-};
-
-userEmailFooter.onblur = function() {
-  myColorFunc(mailReg, userEmailFooter);
-};
-
-userPassword.onblur = function() {
-  myColorFunc(passReg, userPassword);
-};
-
-messageSubject.onblur = function() {
-  myColorFunc(subjReg, messageSubject);
-};
-
-theMessage.onblur = function() {
-  myColorFunc(textAreaReg, theMessage);
-};
-
+myForm.forEach(function(element) {
+  element.addEventListener("blur", newColorFunc, false);
+});
--- css-practice/task_2_dynamic/tabs.js
+++ css-practice/task_2_dynamic/tabs.js
@@ -1,14 +1,13 @@
-/* eslint-disable*/
+/*eslint-disable*/
 function tabulation(event, currentTab) {
-  let description = document.getElementsByClassName('tab-description');
-  for (let i = 0; i < description.length; i++) {
-    description[i].style.display = 'none';
-  }
-  let pointer = document.getElementsByClassName('tab');
-  for (let i = 0; i < pointer.length; i++) {
-    pointer[i].className = pointer[i].className.replace(' active', '');
-  }
+  let description;
+
+  description = Array.from(document.getElementsByClassName('tab-description'));
+
+  description.forEach((element) => {
+    element.style.display = 'none';
+  });
   document.getElementById(currentTab).style.display = 'block';
-  event.currentTarget.className += ' active';
+  event.currentTarget.classList.toggle('active');
 }
 document.getElementById('default-open').click();
