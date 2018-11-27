5c657d62f8c7fe6e00b5307b0d9628c0359030c3
moskdev <molchanovsk@gmail.com>
2018-11-27 16:43:12 +0200

    tab switch reimplentation, name attributes fix

--- axit-interactive/js/script.js
+++ axit-interactive/js/script.js
@@ -8,6 +8,8 @@ trialSubmitForm.addEventListener('submit', validateForm.bind(null, 'free-trial-f
 trialSubmitForm.addEventListener('keyup', validate, true);
 sendMessageFrom.addEventListener('keyup', validate, true);
 sendMessageFrom.addEventListener('submit', validateForm.bind(null, 'send-message'), true);
+trialSubmitForm.addEventListener('focus', validate, true);
+sendMessageFrom.addEventListener('focus', validate, true);
 
 
 const patterns = {
@@ -16,18 +18,22 @@ const patterns = {
   password: /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+-]).{6,20}/,
 };
 
-function validateForm(id) {
+
+function validateForm(id, e) {
   const inputArr = Array.from(document.getElementById(id).children);
 
   for (let i = 0; i < inputArr.length; i++) {
-    if (inputArr[i].classList.contains('error')) {
+    if (!inputArr[i].classList.contains('success')) {
+      e.preventDefault();
       return false;
     }
   }
 }
 
 function validate(e) {
-  const pattern = patterns[e.target.name];
+  const pattern = patterns[e.target.getAttribute('data-form')];
+
+  if (e.target.classList.contains('submit-btn')) return;
 
   if (e.target.id === 'textarea-message') {
     if (e.target.value) {
@@ -37,12 +43,12 @@ function validate(e) {
       e.target.classList.remove('success');
       e.target.classList.add('error');
     }
+    return;
   }
 
   if (!e.target.value) {
     e.target.classList.remove('success');
     e.target.classList.add('error');
-
     return false;
   }
   if (pattern.test(e.target.value)) {
@@ -60,6 +66,7 @@ function tabSwitch(e) {
     'Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes.',
   ];
   const featureTabsArr = featureTabs.children;
+  const tabContent = document.getElementById('feature-content');
   if (!e.target.classList.contains('features--btn')) {
     return;
   }
@@ -70,19 +77,15 @@ function tabSwitch(e) {
     for (let i = 0; i < featureTabsArr.length; i++) {
       featureTabsArr[i].classList.remove('tab-active');
     }
-    e.target.classList.add('tab-active');
-    document.getElementById('features-para').innerText = featureParas[e.target.id.slice(-1)];
-    let myopacity = 0;
-    paraFade();
 
-    function paraFade() {
-      if (myopacity < 1) {
-        myopacity += .03;
-        setTimeout(function () {
-          paraFade();
-        }, 15);
-      }
-      document.getElementById('feature-content').style.opacity = myopacity;
-    }
+    e.target.classList.add('tab-active');
+    tabContent.classList.remove('opacity-1');
+    tabContent.classList.add('opacity-0');
+    setTimeout(function () {
+      document.getElementById('features-para').innerText = featureParas[e.target.id.slice(-1)];
+      tabContent.classList.remove('opacity-0');
+      tabContent.classList.add('opacity-1');
+    }, 300);
   }
 }
+
