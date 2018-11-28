067d67a91d91465e28c314dfd3dd1154c95cf9cf
SofiMat <sofiyamatviishyn@gmail.com>
2018-11-28 17:38:18 +0200

    header complete

---
+++ html-template-3/script.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+(() => {
+  const head = document.getElementById('head');
+  const navigation = document.querySelector('.top-navigation');
+
+  document.addEventListener('scroll', addBackground); 
+  navigation.addEventListener('click', e => addBorder(e.target.id));
+
+  function addBackground() {
+    if(document.documentElement.scrollTop > 900) {
+      head.classList.add('add-background');
+    } else {
+      head.classList.remove('add-background');
+    }
+  }
+
+  const menuItem = document.getElementsByClassName('menu-item');
+
+  function addBorder(id) {
+    const selectedLink = document.getElementById(`${id}`);
+    for (let i = 1; i <= menuItem.length; i++) {
+      const link = document.getElementById(`${i}`);
+      if (selectedLink === link) {
+        link.classList.add('add-border');
+      } else {
+        link.classList.remove('add-border');
+      }
+    }
+  }
+
+})();
+
+
