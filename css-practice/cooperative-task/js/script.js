b042d86845a57c804cf1c03ea5d5874d70e73e23
Andriy Zhukevych <reactive.adnrew@yahoo.com.ua>
2018-11-28 13:07:46 +0200

    implemented validate

--- css-practice/cooperative-task/js/script.js
+++ css-practice/cooperative-task/js/script.js
@@ -1,6 +1,7 @@
 'use strict';
-/* global document */
+/* global document alert */
 (function() {
+  // Background video playback
   const playBtn = document.querySelector('.btn-video');
   const bgVideo = document.querySelector('video');
 
@@ -15,4 +16,50 @@
     playBtn.classList.toggle('play');
   };
   playBtn.addEventListener('click', playState);
+
+  // Form validation
+  const contactForm = document.querySelector('.contact__form');
+  const submitBtn = contactForm.querySelector('[type="submit"]');
+  // select inputs from form
+  const inputsArr = [...contactForm.elements].filter((el) => {
+    return el.type === 'text' || el.type === 'email' || el.type === 'password';
+  });
+  /* input invalid messages
+  const isEmptyMsg = 'Please fill out this field!';
+  const notNameMsg = 'All lowercase letters & minlength 3';
+  const notEmailMsg = 'Write correct email adress';
+  const notPassMsg = 'Password didn\'t match';
+  */
+
+  const checkUserInput = (input) => {
+    const value = input.value;
+    const pattern = new RegExp(input.getAttribute('data-pattern'));
+    const isValid = pattern.test(value);
+    if (isValid) {
+      input.classList.add('valid');
+      input.classList.remove('invalid');
+    } else {
+      input.classList.remove('valid');
+      input.classList.add('invalid');
+    }
+  };
+
+  const submitSwitch = (e) => {
+    const input = e.target;
+    checkUserInput(input);
+    const subState = inputsArr.every((el) => {
+      return el.classList.contains('valid');
+    });
+    if (subState) {
+      submitBtn.disabled = false;
+    } else {
+      submitBtn.disabled = true;
+    }
+  };
+
+  contactForm.addEventListener('input', submitSwitch, true);
+  contactForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Success!!!');
+  });
 })();
