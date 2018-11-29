ae0b9878ec7576ad69596ae6d94f5518d8dbfd2f
Andriy Zhukevych <reactive.adnrew@yahoo.com.ua>
2018-11-29 17:59:20 +0200

    complited accordion with effects

--- css-practice/task3/js/script.js
+++ css-practice/task3/js/script.js
@@ -6,45 +6,56 @@
   // });
   const accordion = document.getElementById('accordion');
   const panelsArr = Array.from(accordion.children);
+  const iconsArr = Array.from(accordion.querySelectorAll('.right'));
+  const imagesArr = Array.from(document.querySelectorAll('.accordion-pic'));
 
   const switchHandler = (e) => {
     e.preventDefault();
     const actionBtn = e.target;
     const iconChev = actionBtn.children[1];
     const activePanel = actionBtn.parentNode.parentNode.parentNode;
-    const openState = isOpenPanel(activePanel);
+    const openState = activePanel.classList.contains('open') ? true : false;
+    displayPanelImage(activePanel);
     closeAllPanels();
     togglePanel(activePanel, openState, iconChev);
   };
 
-  const isOpenPanel = (panel) => {
-    return panel.classList.contains('open') ? true : false;
+  const displayPanelImage = (active) => {
+    const currIndex = panelsArr.indexOf(active);
+    if (!imagesArr[currIndex].classList.contains('in')) {
+      imagesArr.forEach((image) => {
+        image.classList.remove('in');
+      });
+      if (currIndex !== -1) {
+        imagesArr[currIndex].classList.add('in');
+      }
+    }
   };
 
   const closeAllPanels = () => {
-    panelsArr.forEach((el) => {
-      el.classList.remove('open');
-      el.classList.add('close');
-      el.children[1].classList.remove('in');
+    panelsArr.forEach((panel) => {
+      panel.classList.remove('open');
+      panel.classList.add('close');
+    });
+    iconsArr.forEach((icon) => {
+      icon.classList.remove('fa-chevron-up');
+      icon.classList.add('fa-chevron-down');
     });
   };
 
   const togglePanel = (panel, state, icon) => {
-    icon.classList.toggle('fa-chevron-up');
-    icon.classList.toggle('fa-chevron-down');
-    const content = panel.children[1];
     if (!state) {
       panel.classList.remove('close');
       panel.classList.add('open');
-      content.classList.add('in');
+      icon.classList.remove('fa-chevron-down');
+      icon.classList.add('fa-chevron-up');
     } else {
       panel.classList.add('close');
       panel.classList.remove('open');
-      content.classList.remove('in');
     }
   };
 
-  panelsArr.forEach((el, index) => {
+  panelsArr.forEach((el) => {
     el.addEventListener('click', switchHandler);
   });
 })();
