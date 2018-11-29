51923c2125b1524c1457b428e11a0b169efe5180
SofiMat <sofiyamatviishyn@gmail.com>
2018-11-29 18:18:30 +0200

    + accordion without pics

--- html-template-3/script.js
+++ html-template-3/script.js
@@ -1,20 +1,21 @@
 /* eslint-disable */
 (() => {
   const head = document.getElementById('head');
-  const navigation = document.querySelector('.top-navigation');
 
-  document.addEventListener('scroll', addBackground); 
-  navigation.addEventListener('click', e => addBorder(e.target.id));
+  document.addEventListener('scroll', addBackground);
 
   function addBackground() {
-    if(document.documentElement.scrollTop > 0) {
+    if (document.documentElement.scrollTop > 0) {
       head.classList.add('add-background');
     } else {
       head.classList.remove('add-background');
     }
   }
 
+  const navigation = document.querySelector('.top-navigation');
   const menuItem = document.getElementsByClassName('menu-item');
+
+  navigation.addEventListener('click', e => addBorder(e.target.id));
 
   function addBorder(id) {
     const selectedLink = document.getElementById(`${id}`);
@@ -25,6 +26,22 @@
       } else {
         link.classList.remove('add-border');
       }
+    }
+  }
+
+  const accItem = document.getElementsByClassName('drop-item');
+  const accHD = document.getElementsByClassName('drop-item-header');
+  for (let i = 0; i < accHD.length; i++) {
+    accHD[i].addEventListener('click', toggleItem, false);
+  }
+
+  function toggleItem() {
+    const itemClass = this.parentNode.className;
+    for (let i = 0; i < accItem.length; i++) {
+      accItem[i].className = 'drop-item close';
+    }
+    if (itemClass === 'drop-item close') {
+      this.parentNode.className = 'drop-item open';
     }
   }
 
