4566b3f920ccb397943c86e2524528dfc95904d3
moskdev <molchanovsk@gmail.com>
2018-12-03 19:02:03 +0200

    gallery implementation , accordion fix

--- css-practice/task-3/js/script.js
+++ css-practice/task-3/js/script.js
@@ -1,3 +1,4 @@
+/* global document*/
 const conts = document.querySelectorAll('.services-info-title');
 const eks = document.querySelectorAll('.services-info-text');
 
@@ -6,35 +7,46 @@ conts.forEach((el) => {
 });
 
 function toggleShow(e) {
+
+  const textClassList = e.currentTarget.nextElementSibling.classList;
+  conts.forEach((el) => {
+    if (el !== e.currentTarget) {
+      el.querySelector('i').classList.remove('rotate-180');
+    }
+  });
+
+  e.currentTarget.querySelector('i').classList.toggle('rotate-180');
+
   if (e.currentTarget.nextElementSibling.classList.contains('height-0')) {
     eks.forEach((el) => {
       el.classList.remove('height-auto');
       el.classList.add('height-0');
     });
   } else {
-    e.currentTarget.nextElementSibling.classList.remove('height-auto');
-    e.currentTarget.nextElementSibling.classList.add('height-0');
+    textClassList.remove('height-auto');
+    textClassList.add('height-0');
     return;
   }
-  e.currentTarget.nextElementSibling.classList.toggle('height-0');
-  e.currentTarget.nextElementSibling.classList.toggle('height-auto');
+  textClassList.toggle('height-0');
+  textClassList.toggle('height-auto');
 }
+
+// carousel
 
 const rightArrows = document.querySelectorAll('.fa-angle-right');
 const leftArrows = document.querySelectorAll('.fa-angle-left');
-const carouContainer = document.querySelector('.carousel-container');
 
 leftArrows.forEach((el) => {
   el.addEventListener('click', carouselPrev);
-})
+});
 rightArrows.forEach((el) => {
   el.addEventListener('click', carouselNext);
-})
+});
 
 function carouselNext(e) {
   let active = e.currentTarget.parentElement.querySelector('.active-carou-item');
 
-  if(!active.nextElementSibling) {
+  if (!active.nextElementSibling) {
     active.classList.remove('active-carou-item');
     active.classList.add('next-carou-item');
     active.previousElementSibling.classList.remove('prev-carou-item');
@@ -42,8 +54,8 @@ function carouselNext(e) {
     active.previousElementSibling.previousElementSibling.classList.remove('prev-carou-item');
     active.previousElementSibling.previousElementSibling.classList.add('active-carou-item');
     return;
-  };
-  
+  }
+
   active.classList.remove('active-carou-item');
   active.classList.add('prev-carou-item');
   active.nextElementSibling.classList.remove('next-carou-item');
@@ -53,7 +65,7 @@ function carouselNext(e) {
 function carouselPrev(e) {
   let active = e.currentTarget.parentElement.querySelector('.active-carou-item');
 
-  if(!active.previousElementSibling) {
+  if (!active.previousElementSibling) {
     active.classList.remove('active-carou-item');
     active.classList.add('prev-carou-item');
     active.nextElementSibling.classList.remove('next-carou-item');
@@ -61,10 +73,33 @@ function carouselPrev(e) {
     active.nextElementSibling.nextElementSibling.classList.remove('next-carou-item');
     active.nextElementSibling.nextElementSibling.classList.add('active-carou-item');
     return;
-  };
+  }
 
   active.classList.remove('active-carou-item');
   active.classList.add('next-carou-item');
   active.previousElementSibling.classList.remove('prev-carou-item');
   active.previousElementSibling.classList.add('active-carou-item');
+}
+
+// modals
+
+const gallery = document.querySelector('.gallery-container');
+
+gallery.addEventListener('click', showPopup);
+
+function showPopup(e) {
+  if (!e.target.nextElementSibling) return;
+  const modal = e.target.nextElementSibling.nextElementSibling;
+  const galleryImage = e.target.nextElementSibling;
+  const modalImg = modal.querySelector('.modal-content');
+
+  modal.classList.remove('none');
+  modal.classList.add('block');
+  modalImg.src = galleryImage.src;
+  modal.addEventListener('click', closePopup);
+}
+
+
+function closePopup(e) {
+  document.querySelector('.block').classList.replace('block', 'none');
 }
