2a6bd41fbe7ae20ba651901596dcb87c6f9af9a2
Andrew Zhukevych <hipnos.az@gmail.com>
2018-12-03 23:22:05 +0200

    Last updates

--- css-practice/task3/js/popup.js
+++ css-practice/task3/js/popup.js
@@ -9,21 +9,29 @@
     const imageUrl = image.src;
     const modalTemplate = `
       <div class="modal__window fadeIn animated">
-        <img src="${imageUrl}" alt=""/>
+        <figure>
+          <img src="${imageUrl}" alt=""/>
+        </figure>
+        <span class="btn-close" aria-role="button" tabindex="0">&times;</span>
       </div>
       <div class="modal__overlay"></div>
     `;
 
     // Append to parent Element
-    modal.classList.add('modal', 'center-item');
+    modal.classList.add('modal');
     modal.innerHTML = modalTemplate;
     gallery.append(modal);
 
     // Select modal__overlay & btn-close
     const modOverlay = modal.querySelector('.modal__overlay');
-
+    const btnClose = modal.querySelector('.btn-close');
+    // Event Listeners in modal-view
     gallery.removeEventListener('click', popupShow);
     modOverlay.addEventListener('click', removeModal.bind(null, modal));
+    btnClose.addEventListener('click', removeModal.bind(null, modal));
+    btnClose.addEventListener('keyup', (e) => {
+      if (e.keyCode === 13) removeModal(modal, e);
+    });
   };
 
   // Remove modal window
