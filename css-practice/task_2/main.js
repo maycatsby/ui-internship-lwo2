6bc910a5266c7cb98a240298a0b2dfcf7b7577db
NFalinska <nataliia.falinska@gmail.com>
2018-12-04 14:41:57 +0200

    some fix

--- css-practice/task_2/main.js
+++ css-practice/task_2/main.js
@@ -1,45 +1,43 @@
-/* global  document, window */
-window.onload = function() {
-  const video = document.getElementById('my-video');
-  const btnVideo = document.getElementById('play');
-  if (btnVideo) {
-    btnVideo.addEventListener('click', togglePlay, false);
-  }
+/* global  document */
+const video = document.getElementById('my-video');
+const btnVideo = document.getElementById('play');
+if (btnVideo) {
+  btnVideo.addEventListener('click', togglePlay, false);
+}
 
-  function togglePlay() {
+function togglePlay() {
     video.paused ? playVideo() : pauseVideo();
-  }
+}
 
-  function playVideo() {
-    video.play();
-    btnVideo.classList.remove('play');
-    btnVideo.classList.add('paused');
-  }
+function playVideo() {
+  video.play();
+  btnVideo.classList.remove('play');
+  btnVideo.classList.add('paused');
+}
 
-  function pauseVideo() {
-    video.pause();
-    btnVideo.classList.remove('paused');
-    btnVideo.classList.add('play');
-  }
+function pauseVideo() {
+  video.pause();
+  btnVideo.classList.remove('paused');
+  btnVideo.classList.add('play');
+}
 
-  const loginFormInpunts = document.forms['main-form'].getElementsByTagName('input');
-  document.forms['main-form'].addEventListener('keyup', validate);
-  
-  function validate(event) {
-    if (!event.target.validity.valid || !event.target.value) {
-      event.target.classList.add('invalid');
-    } else {
-      event.target.classList.remove('invalid');
-      event.target.classList.add('valid');
-    }
-    checkItem();
-  }
+const loginFormInpunts = document.forms['main-form'].getElementsByTagName('input');
+document.forms['main-form'].addEventListener('keyup', validate);
 
-  function checkItem() {
-    const sendBtn = document.getElementById('send-btn');
-    const isValid = [...loginFormInpunts].every((el) => el.classList.contains('valid'));
-    if (isValid) {
-      sendBtn.classList.remove('disabled');
-    }
+function validate(event) {
+  if (!event.target.validity.valid || !event.target.value) {
+    event.target.classList.add('invalid');
+  } else {
+    event.target.classList.remove('invalid');
+    event.target.classList.add('valid');
   }
-};
+  checkItem();
+}
+
+function checkItem() {
+  const sendBtn = document.getElementById('send-btn');
+  const isValid = [...loginFormInpunts].every((el) => el.classList.contains('valid'));
+  if (isValid) {
+    sendBtn.classList.remove('disabled');
+  }
+}
