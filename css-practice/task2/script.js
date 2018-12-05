6faa6c9ba90ca790428f673fed6ee92b9f07a107
SofiMat <sofiyamatviishyn@gmail.com>
2018-12-05 15:53:40 +0200

    Complete module realization

--- css-practice/task2/script.js
+++ css-practice/task2/script.js
@@ -1,8 +1,7 @@
 /* eslint-disable quotes */
 /* eslint max-len: ["error", { "code": 120, "tabWidth": 2 }]*/
 
-import {videoModuleApi} from "ui-internship-lwo2/css-practice/task2/videoModule.js";
-// import {formValidationModuleApi} from "ui-internship-lwo2/css-practice/task2/formValidationModule.js";
+import videoModuleApi from "./videoModule.js";
 import formValidationModuleApi from "./formValidationModule.js";
 
 videoModuleApi.init();
