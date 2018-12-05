22caa337ce5ab7e8c6fdfc2990aca5832adcc22b
Andriy Zhukevych <reactive.adnrew@yahoo.com.ua>
2018-12-05 14:31:40 +0200

    Fixed exports

--- css-practice/cooperative-task/js/video-bg.js
+++ css-practice/cooperative-task/js/video-bg.js
@@ -1,24 +1,23 @@
 /* global document */
-const videoModule = (() => {
-  const playBtn = document.querySelector('.btn-video');
-  const bgVideo = document.querySelector('video');
 
-  const playState = (evt) => {
-    evt.preventDefault();
-    if (playBtn.classList.contains('pause')) {
-      bgVideo.play();
-    } else {
-      bgVideo.pause();
-    }
-    playBtn.classList.toggle('play');
-    playBtn.classList.toggle('pause');
-  };
+const playBtn = document.querySelector('.btn-video');
+const bgVideo = document.querySelector('video');
 
-  return {
-    init: () => {
-      playBtn.addEventListener('click', playState);
-    },
-  };
-})();
+const playState = (evt) => {
+  evt.preventDefault();
+  if (playBtn.classList.contains('pause')) {
+    bgVideo.play();
+  } else {
+    bgVideo.pause();
+  }
+  playBtn.classList.toggle('play');
+  playBtn.classList.toggle('pause');
+};
 
-export default videoModule;
+const videoAPI = {
+  init: () => {
+    playBtn.addEventListener('click', playState);
+  },
+};
+
+export default videoAPI;
