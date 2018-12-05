b69ac241548326b805f3e676688ee349ea984383
Artur Kostiuk <artukos54@gmail.com>
2018-12-05 14:31:13 +0200

    part-2

--- css-practice/task_2_2/videoPlayer.js
+++ css-practice/task_2_2/videoPlayer.js
@@ -1,35 +1,35 @@
 /* global document */
 
-const videoPlayer = (() => {
-  const dNone = 'disp-none';
-  const hideEl = (el) => {
-    el.classList.add(dNone);
-  };
-  const showEl = (el) => {
-    el.classList.remove(dNone);
-  };
-  const video = document.querySelector('video');
-  const playVideo = document.querySelector('.video-player');
-  const continueVideo = document.querySelector('.fa-play-circle');
-  const pauseVideo = document.querySelector('.fa-pause-circle');
 
-  const playVideoHandler = () => {
-    if (video.paused) {
-      hideEl(continueVideo);
-      showEl(pauseVideo);
-      video.play();
-    } else {
-      showEl(continueVideo);
-      hideEl(pauseVideo);
-      video.pause();
-    }
-  };
+const dNone = 'disp-none';
+const hideEl = (el) => {
+  el.classList.add(dNone);
+};
+const showEl = (el) => {
+  el.classList.remove(dNone);
+};
+const video = document.querySelector('video');
+const playVideo = document.querySelector('.video-player');
+const continueVideo = document.querySelector('.fa-play-circle');
+const pauseVideo = document.querySelector('.fa-pause-circle');
 
-  return {
-    init: () => {
-      playVideo.addEventListener('click', playVideoHandler);
-    },
-  };
-})();
+const playVideoHandler = () => {
+  if (video.paused) {
+    hideEl(continueVideo);
+    showEl(pauseVideo);
+    video.play();
+  } else {
+    showEl(continueVideo);
+    hideEl(pauseVideo);
+    video.pause();
+  }
+};
 
-videoPlayer.init();
+const videoAPI = {
+  init: () => {
+    playVideo.addEventListener('click', playVideoHandler);
+  },
+};
+
+
+export default videoAPI;
