12c914f64df520a24caf7397325bfa70c5087e4b
justyna <jusdan92@gmail.com>
2019-05-03 19:36:13 +0200

    changes after review

--- tasks/24_index-to-insert/index-to-insert.js
+++ tasks/24_index-to-insert/index-to-insert.js
@@ -1,6 +1,6 @@
 export const getIndexToInsert = (arr, arg) => {
   let sortArr = arr.sort((a, b) => a - b);
-  let indexNum = sortArr.map((cur) => cur >= arg ? true : false).indexOf(true);
+  let indexNum = sortArr.map((cur) => cur >= arg).indexOf(true);
   return indexNum !== (-1) ? indexNum : sortArr.length;
 };
 
