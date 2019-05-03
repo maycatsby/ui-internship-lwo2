6563562c53153729deefd29cc2eca30cd8f1e89a
justyna <jusdan92@gmail.com>
2019-05-03 19:36:13 +0200

    changes after review

--- tasks/21_check-letters/check-letters.js
+++ tasks/21_check-letters/check-letters.js
@@ -1,8 +1,7 @@
 export const checkLetters = (arr) => {
   const lowerCaseArr = arr.map((cur) => cur.toLowerCase());
-  const secondElArr = lowerCaseArr[1].split('').map(
-      (cur) => lowerCaseArr[0].includes(cur) ? true : false);
-  let isIncluded = secondElArr.indexOf(false) === (-1) ? true : false;
-  return isIncluded;
+  const secondElArr = lowerCaseArr[1].split('')
+      .map((cur) => lowerCaseArr[0].includes(cur));
+  return secondElArr.indexOf(false) === (-1);
 };
 
