73d769910e151ec68384d49813cab8cb6a46344c
justyna <jusdan92@gmail.com>
2019-05-07 18:27:38 +0200

    changes after review

--- tasks/41_sort-by-bits/sort-by-bits.js
+++ tasks/41_sort-by-bits/sort-by-bits.js
@@ -1,18 +1,27 @@
 export const sortByBits = (arr) => {
-  arr.forEach((cur, index) => {
-    let nextNumber = Math.abs(cur);
-    let binaryFormat = cur >= 0 ? [] : [1];
-
-    while (nextNumber > 0) {
-      if (nextNumber % 2 === 1) {
-        binaryFormat.push(1);
-      }
-      nextNumber = Math.floor(nextNumber / 2);
+  let arrSort = arr.sort((a, b) => {
+    let aOne = a.toString(2).replace(/[0]/g, '').length;
+    let bOne = b.toString(2).replace(/[0]/g, '').length;
+    if (aOne > bOne) {
+      return 1;
+    } else if (aOne < bOne) {
+      return -1;
+    } else {
+      return a - b;
     }
-    arr[index] = {value: cur, length: binaryFormat.length};
   });
-  arr.sort((a, b) => a.length - b.length || a.value - b.value)
-      .forEach((cur, index) => arr[index] = cur.value);
-  return arr;
+  return arrSort;
 };
 
+
+/* Second solution:
+
+ export const sortByBits = (arr) => {
+  let arrOfOne = arr.map((cur) => {
+    return {val: cur, length: cur.toString(2).replace(/[0]/g, '')};
+  });
+  return arrOfOne.sort((a, b) => (a.length - b.length || a.val - b.val))
+      .map((cur) => cur.val);
+};
+
+*/
--- tasks/49_cat-and-dog-years/cat-and-dog-years.js
+++ tasks/49_cat-and-dog-years/cat-and-dog-years.js
@@ -1,18 +1,12 @@
 export const humanYearsCatYearsDogYears = (humanAge) => {
-  const ages = [humanAge, 0, 0];
   if (humanAge < 1) {
-    return ages;
+    return [0, 0, 0];
   }
-  for (let i = 1; i < ages.length; i++) {
-    let threeOrMore = i === 1 ? 4 : 5;
-    switch (humanAge) {
-      case 1:
-        ages[i] = 15; break;
-      case 2:
-        ages[i] = 15 + 9; break;
-      default:
-        ages[i] = 15 + 9 + (humanAge - 2) * threeOrMore; break;
-    }
-  }
-  return ages;
+  let two = humanAge > 1 ? 1 : 0;
+  let threeOrMore = humanAge > 2 ? (humanAge - 2) : 0;
+  let catAge = 15 + 9 * two + 4 * threeOrMore;
+  let dogAge = 15 + 9 * two + 5 * threeOrMore;
+
+  return [humanAge, catAge, dogAge];
 };
+
