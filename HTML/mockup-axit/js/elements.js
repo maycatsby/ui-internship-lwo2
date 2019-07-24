397f7d34fb4389a02ae9ce2d719bd324e4d336b0
justyna <jusdan92@gmail.com>
2019-07-24 14:34:22 +0200

    mockup-axit - task 2 JS

--- HTML/mockup-axit/js/elements.js
+++ HTML/mockup-axit/js/elements.js
@@ -1,7 +1,14 @@
-/* eslint-disable no-undef */
-export const elements = {
-  formTrialForm: document.querySelector('.form-trial'),
-  formContactForm: document.querySelector('.form-contact'),
-  sentTrialForm: document.querySelector('.form-trial--submitted'),
-  sentContactForm: document.querySelector('.form-contact--submitted'),
-};
+export const elementsLoaded = new Promise((res) => {
+  document.addEventListener('DOMContentLoaded', () => {
+    const elements = {
+      formTrialForm: document.querySelector('.form-trial'),
+      formContactForm: document.querySelector('.form-contact'),
+      sentTrialForm: document.querySelector('.form-trial--submitted'),
+      sentContactForm: document.querySelector('.form-contact--submitted'),
+      tabs: document.querySelectorAll('.tab'),
+      tabTitle: document.querySelector('.section-tabs__heading'),
+      tabParagraph: document.querySelector('.section-tabs__paragraph'),
+    };
+    res(elements);
+  });
+});
