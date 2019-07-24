a2b9427f44e8a8c69b4c4cb62f1c35ad128f3522
justyna <jusdan92@gmail.com>
2019-07-24 15:37:44 +0200

    mockup-corpo task 4JS

--- HTML/mockup-corporate/js/task3.js
+++ HTML/mockup-corporate/js/task3.js
@@ -1,46 +1,52 @@
-import {elements} from './elements.js';
+import {elementsLoaded} from './elements.js';
 import {patterns} from './patterns.js';
 
-const inputNameArr = [
-  elements.nameInput,
-  elements.emailInput,
-  elements.passwordInput,
-];
+export const formValidator = () => {
+  elementsLoaded.then((data) => {
+    const elements = data;
 
-const patternArr = [patterns.name, patterns.email, patterns.password];
+    const inputNameArr = [
+      elements.nameInput,
+      elements.emailInput,
+      elements.passwordInput,
+    ];
 
-const validator = (input, pattern) => {
-  let inputValue = input.value;
-  let isValid = pattern.test(inputValue);
+    const patternArr = [patterns.name, patterns.email, patterns.password];
 
-  input.classList.remove('input--invalid');
+    const validator = (input, pattern) => {
+      let inputValue = input.value;
+      let isValid = pattern.test(inputValue);
 
-  if (!isValid) {
-    input.classList.add('input--invalid');
-  }
+      input.classList.remove('input--invalid');
 
-  return isValid;
+      if (!isValid) {
+        input.classList.add('input--invalid');
+      }
+
+      return isValid;
+    };
+
+    const validArr = [false, false, false];
+
+    elements.form.addEventListener('input', (e) => {
+      let index = e.target.dataset.index;
+      let validValue = validator(inputNameArr[index], patternArr[index]);
+      validArr[index] = validValue;
+
+      elements.btn.classList.remove('btn--disabled');
+
+      if (validArr.includes(false)) {
+        elements.btn.classList.add('btn--disabled');
+      }
+    });
+
+    elements.form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      if (!validArr.includes(false)) {
+        elements.form.classList.add('u-hidden');
+        elements.sentForm.classList.add('u-show');
+      }
+    });
+  });
 };
-
-const validArr = [false, false, false];
-
-elements.form.addEventListener('input', (e) => {
-  let index = e.target.dataset.index;
-  let validValue = validator(inputNameArr[index], patternArr[index]);
-  validArr[index] = validValue;
-
-  elements.btn.classList.remove('btn--disabled');
-
-  if (validArr.includes(false)) {
-    elements.btn.classList.add('btn--disabled');
-  }
-});
-
-elements.form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  if (!validArr.includes(false)) {
-    elements.form.classList.add('u-hidden');
-    elements.sentForm.classList.add('u-show');
-  }
-});
